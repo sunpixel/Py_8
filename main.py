363eefc6b367304dae48f3d81f2209ff0cb58aee
SunPixel <tarakinnikita@gmail.com>
2024-12-08 05:01:26 +0300

    Ex 3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''Main'''
 from fastapi import FastAPI, Path
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 
 app = FastAPI()
 
@@ -11,9 +11,9 @@ def read_root():
     return HTMLResponse(content=html_content)
 
 @app.get('/greet/{name}')
-def name(name):
+def name(name1):
     '''Name fuction'''
-    content = f'<h1>Hello, {name}<h1>'
+    content = f'<h1>Hello, {name1}<h1>'
     return HTMLResponse(content=content)
 
 @app.get('/search/{string}')
@@ -21,3 +21,25 @@ def searcg(string: str = Path(min_length=2, max_length=40)):
     '''Search query'''
     html = f'<h2>You searched: {string}<h2>'
     return HTMLResponse(content=html)
+
+@app.get('/json')
+def jsonsender():
+    '''Json sender function'''
+    json = {
+        'name': 'Nikita',
+        'age': '21',
+        'hobbies': 'programming'
+    }
+    return json
+
+@app.get('/file')
+def file_sender():
+    '''A file sending function'''
+    return FileResponse("Public/text.txt",
+                        filename="A_Text_File.txt",
+                        media_type="application/octet-stream")
+
+@app.get('/redirect')
+def redirection_handler():
+    '''a function that will redirect'''
+    return RedirectResponse('/', status_code=308)
